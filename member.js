c4213f01d34b453ccda060d1aa863eb68186b755
ivovila <138324273+ivovila@users.noreply.github.com>
2023-12-05 03:48:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(memberSkills) {
+    // Do something with the member's skills
+    console.log(`Member's skills: ${memberSkills.join(', ')}`);
+    
+    // You can add more logic here based on your requirements
+  }
